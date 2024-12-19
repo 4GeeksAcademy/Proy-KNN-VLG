a53e661acf3f6f967ee27d2c913f34d84a696da4
VLGuazzelli <victoria.guazzelli@gmail.com>
2024-12-19 21:29:50 +0100

    Sigo con prueba

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-from utils import db_connect
-engine = db_connect()
-
 # your code here
+from pickle import load
+from flask import Flask,request,render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"

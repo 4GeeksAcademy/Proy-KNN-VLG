f7e4ed50c625296aa1d38a0ce6813b7544617318
Victoria Guazzelli <166502413+VLGuazzelli@users.noreply.github.com>
2025-01-06 10:22:57 +0000

    En proceso

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@
 from pickle import load
 from flask import Flask,request,render_template
 from sklearn.feature_extraction.text import TfidfVectorizer
+# no supervisado
+from sklearn.neighbors import NearestNeighbors
 
 app = Flask(__name__)
 
+model = load(open("/workspaces/Proy-KNN-VLG/src/KNN_recomendations_movies.sav", "rb"))
+
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
